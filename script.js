c7fc1f2811e886d5150782082612231bc7e87d4f
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-10-07 18:18:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	for(let i=0; i<text.length; i++){
+		if(text[i] !== ' ') // char blank nhi h mean kuch na kuch value h 
+		{
+			return text[i];
+		}
+		return ''
+	}
+	
 }
 
 // Do not change the code below
